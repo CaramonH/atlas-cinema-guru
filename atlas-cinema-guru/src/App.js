75b0bffbd9663595b2f1aa86c70fb1df09038b03
CaramonH <caramonh@yahoo.com>
2024-07-02 13:50:05 -0500

    rendering a button, label, and input bar

--- atlas-cinema-guru/src/App.js
+++ atlas-cinema-guru/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Button from './components/general/Button.js';
+import {useState} from 'react';
+import Input from './components/general/Input.js';
 
 function App() {
+  // Input Use State
+  const [inputValue, setInputValue] = useState ("");
+
+  // Input Event Handler
   return (
     <div className="App">
       <div>
         <Button label="Load More..."/>
+      </div>
+      <div>
+        <Input
+        label="Please Enter Your Name..."
+        type="text"
+        value={inputValue}
+        setValue={setInputValue}
+        />
       </div>
     </div>
   );
