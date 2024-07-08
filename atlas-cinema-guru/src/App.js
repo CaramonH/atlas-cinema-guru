6cb3bf3488350fef514abdb7a28611c6137ce675
CaramonH <caramonh@yahoo.com>
2024-07-08 14:57:14 -0500

    LOGGED IN :D

--- atlas-cinema-guru/src/App.js
+++ atlas-cinema-guru/src/App.js
@@ -1,87 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-import {useState, useEffect} from 'react';
-import Input from './components/general/Input.js';
-import Button from './components/general/Button.js';
-import SearchBar from './components/general/SearchBar.js';
-import SelectInput from './components/general/SelectInput.js';
 import Dashboard from './routes/dashboard/Dashboard';
 import Authentication from './routes/auth/Authentication';
 
 function App() {
-  // Input Use State
-  const [inputValue, setInputValue] = useState("");
-  // Search Bar Use State
-  const [title, setTitle] = useState("");
-  // Selected Input Use State
-  const [selectedOption, setSelectedOption] = useState("");
-  // Login State
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userUsername, setUserUsername] = useState("");
 
-  // Select Input Options
-  const options = [
-    { label: 'Default', value: '1' },
-    { label: 'Latest', value: '2' },
-    { label: 'Oldest', value: '3' },
-    { label: 'Highest Rated', value: '4' },
-    { label: 'Lowest Rated', value: '5' },
-  ];
-
-  // useEffect to check for access token and authenticate user
   useEffect(() => {
-    const accessToken = localStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem("accessToken");
+    console.log(accessToken);
+
     if (accessToken) {
-      fetch('/api/auth/', {
-        method: 'POST',
+      axios.post('http://localhost:8000/api/auth/', {}, {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${accessToken}`
+          Authorization: `Bearer ${accessToken}`
         }
       })
-      .then(response => response.json())
-      .then(data => {
+      .then(response => {
+        setUserUsername(response.data.username);
         setIsLoggedIn(true);
-        setUserUsername(data.username);
       })
       .catch(error => {
-        console.error('Authentication failed:', error);
-      });
+        console.log('Authentication error:', error.response ? error.response.data : error);
+        // localStorage.removeItem("accessToken");
+        // setIsLoggedIn(false);
+      })
     }
   }, []);
 
-  // Render based on isLoggedIn state
-  if (isLoggedIn) {
-    return <Dashboard username={userUsername} />;
-  }
-
   return (
-    <div className="App">
-      <Authentication />
-      <div>
-        <SearchBar 
-          title={title}
-          setTitle={setTitle}/>
-      </div>
-      <div>
-        <Button label="Load More..."/>
-      </div>
-      <div>
-        <Input 
-          label="Please enter a name..." 
-          type="text" 
-          value={inputValue} 
-          setValue={setInputValue}/>
-      </div>
-      <div>
-        <SelectInput
-          label="Choose an option"
-          options={options}
-          value={selectedOption}
-          setValue={setSelectedOption} />
-      </div>
+    <div className={`App ${isLoggedIn ? 'dashboard-view' : 'authentication-view'}`}>
+      {isLoggedIn ? <Dashboard
+        userUsername={userUsername}
+        setIsLoggedIn={setIsLoggedIn}
+      /> : <Authentication
+        setIsLoggedIn={setIsLoggedIn}
+        setUserUsername={setUserUsername}
+      />}
     </div>
   );
 }
-
 
 export default App;
--- atlas-cinema-guru/src/routes/auth/Authentication.js
+++ atlas-cinema-guru/src/routes/auth/Authentication.js
@@ -1,50 +1,46 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import './auth.css';
 import Button from '../../components/general/Button';
+import React, { useState } from 'react';
 import Login from './Login';
 import Register from './Register';
-import './auth.css';
+import axios from 'axios';
 
 const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
-  const [isLogin, setIsLogin] = useState(true);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [_switch, set_switch] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleToggle = () => {
-    setIsLogin(!isLogin);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (onSubmit) => {
+    onSubmit.preventDefault();
 
     // Basic validation
     if (!username.trim() || !password.trim()) {
-      setError('Username and password are required.');
-      return;
+      setError("Username and password are required.");
+      return; // Stop the function if validation fails
     }
 
-    const endpoint = isLogin
-      ? 'http://localhost:8000/api/auth/login'
-      : 'http://localhost:8000/api/auth/register';
+    const endpoint = _switch ? 'http://localhost:8000/api/auth/login' : 'http://localhost:8000/api/auth/register';
+    console.log({ username, password });
 
     try {
       const response = await axios.post(endpoint, { username, password });
+
       const { accessToken } = response.data;
 
       if (accessToken) {
-        localStorage.setItem('accessToken', accessToken);
-        setUserUsername(username);
+        localStorage.setItem('accessToken', accessToken); // Store the token
+        console.log(typeof setUserUsername);
+        setUserUsername(username); // Update username
         setIsLoggedIn(true);
       }
     } catch (error) {
-      const errorMessage =
-        error.response && error.response.data && error.response.data.message
-          ? error.response.data.message
-          : error.message;
-      setError(errorMessage);
+      const errorMessage = error.response && error.response.data && error.response.data.message
+                      ? error.response.data.message
+                      : error.message;
+      setError(errorMessage); // Update the error state with the new message
       console.error('Authentication error:', errorMessage);
-    }
+      }
   };
 
   return (
@@ -53,17 +49,17 @@ const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
         <Button
           label="Sign In"
           type="button"
-          className={isLogin ? 'light-red' : 'dark-red'}
-          onClick={handleToggle}
+          className={_switch ? "light-red" : "dark-red"}
+          onClick={() => set_switch(true)}
         />
         <Button
           label="Sign Up"
           type="button"
-          className={isLogin ? 'dark-red' : 'light-red'}
-          onClick={handleToggle}
+          className={_switch ? "dark-red" : "light-red"}
+          onClick={() => set_switch(false)}
         />
       </div>
-      {isLogin ? (
+      {_switch ? (
         <Login
           username={username}
           password={password}
@@ -80,7 +76,7 @@ const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
       )}
       {error && <div className="error-message">{error}</div>}
     </form>
-  );
-};
+  )
+}
 
 export default Authentication;
